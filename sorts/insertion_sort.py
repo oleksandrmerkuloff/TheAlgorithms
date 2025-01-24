bd86c004eb634f3ea51237c7209a830fdd21e4a1
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-01-24 23:57:26 +0200

    insertion sort

---
+++ sorts/insertion_sort.py
@@ -0,0 +1,21 @@
+import random
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1
+
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+    return array
+
+
+if __name__ == '__main__':
+    array = random.choices(range(1, 101), k=10)
+    print(array)
+    print(insertion_sort(array))
+
+    assert insertion_sort(array) == sorted(array)
