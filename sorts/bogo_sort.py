29cbeb89f124954598437aa2eb50f90c109f741e
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-02-08 23:39:06 +0200

    add bogo sort

---
+++ sorts/bogo_sort.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+
+def bogo(array):
+    def is_sorted(array):
+        for num in range(len(array) - 1):
+            if array[num] > array[num + 1]:
+                return False
+        return True
+
+    while is_sorted(array) is False:
+        random.shuffle(array)
+    return array
+
+
+if __name__ == '__main__':
+    array = random.choices(range(1, 101), k=10)
+    print(array)
+    started = time.time()
+    print(bogo(array))
+    finish = time.time()
+    print("Wasted time: %s" % (finish - started))
+    assert bogo(array) == sorted(array)
